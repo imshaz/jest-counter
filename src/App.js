8a05d002e5ac17bb8133eab2011b3e8ea121fb20
imshaz <imshazbangash@gmail.com>
2021-04-04 19:35:18 +0500

    added test for decrement button

--- src/App.js
+++ src/App.js
@@ -1,28 +1,64 @@
-
-import React , {Component} from "react"
-import './App.css';
-
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
 
-    this.state={
-      counter:0
-    }
+    this.state = {
+      counter: 0,
+      error: {
+        countLessThanZero: "",
+      },
+    };
   }
-  render(){
-    const {counter}= this.state
+
+  increment = () => {
+    this.setState((prevState, props) => {
+      if (prevState.counter === 0) {
+        return {
+          error: { countLessThanZero: "" },
+          counter: prevState.counter + 1,
+        };
+      }
+      return { counter: prevState.counter + 1 };
+    });
+  };
+  decrement = () => {
+    this.setState((prevState, props) => {
+      if (prevState.counter !== 0) {
+        return { counter: prevState.counter - 1 };
+      } else {
+        this.setState({
+          error: { countLessThanZero: "Counter cannot be less than zero" },
+        });
+      }
+    });
+  };
+  render() {
+    const {
+      counter,
+      error: { countLessThanZero },
+    } = this.state;
     return (
       <div data-test="app" className="App">
-       <h1> Counter app </h1>
-      <h3 data-test="counter-display">current counter is {counter}</h3>
-      <button onClick={()=>this.setState((prevState, props)=>{
-        return {counter:prevState.counter+1}
-      })} data-test="increment-btn">Increment</button>     
-    </div>
-  );
-}
+        <h1> Counter app </h1>
+        <h3 data-test="counter-display">current counter is {counter}</h3>
+        <h6>{countLessThanZero}</h6>
+        <button onClick={() => this.increment()} data-test="increment-btn">
+          Increment
+        </button>
+
+        <button
+          data-test="decrement-btn"
+          onClick={() => this.decrement()}
+          data-test="decrement-btn"
+        >
+          Decrement
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,49 +1,70 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-import Enzyme, {shallow} from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-const setup =(props={}, state=null)=>{
-  return shallow(<App {...props}/>);
-}
+const setup = (props = {}, state = null) => {
+  return shallow(<App {...props} />);
+};
 
-const findByDataTest=(wrapper, datatest)=>{
-  return wrapper.find(`[data-test='${datatest}']`)
-}
-test(`render without any error`, ()=>{
+const findByDataTest = (wrapper, datatest) => {
+  return wrapper.find(`[data-test='${datatest}']`);
+};
+test(`render without any error`, () => {
   const wrapper = setup();
-  const app= findByDataTest(wrapper, 'app');
-  expect(app.length).toBe(1)
-})
+  const app = findByDataTest(wrapper, "app");
+  expect(app.length).toBe(1);
+});
 
-
-test('render incremement button', ()=>{
+test("render incremement button", () => {
   const wrapper = setup();
-  const IncrementBtn = findByDataTest(wrapper, 'increment-btn');  
-  expect(IncrementBtn.length).toBe(1)
-})
-test('render conter placeholder', ()=>{
-
+  const IncrementBtn = findByDataTest(wrapper, "increment-btn");
+  expect(IncrementBtn.length).toBe(1);
+});
+test("render conter placeholder", () => {
   const wrapper = setup();
-  const countDisplay=  findByDataTest(wrapper, 'counter-display');
-  expect(countDisplay.length).toBe(1)
-})
-test('counter start at 0', ()=>{
-  const wrapper = setup(); 
-  const {counter} = wrapper.state();
-  expect(counter).toBe(0)
+  const countDisplay = findByDataTest(wrapper, "counter-display");
+  expect(countDisplay.length).toBe(1);
+});
 
-})
-test('clicking button increment counter display', ()=>{
-  const wrapper=setup(); 
-  const {counter}=wrapper.state; 
+test("counter start at 0", () => {
+  const wrapper = setup();
+  const { counter } = wrapper.state();
+  expect(counter).toBe(0);
+});
 
-  const incrementBtn =findByDataTest(wrapper, 'increment-btn');
-  incrementBtn.simulate('click'); 
-  incrementBtn.simulate('click');
-  incrementBtn.simulate('click');
-  expect(wrapper.state().counter).toBe(3)
-})
+test("clicking button increment counter display", () => {
+  const wrapper = setup();
+  const { counter } = wrapper.state();
+  const incrementBtn = findByDataTest(wrapper, "increment-btn");
+  incrementBtn.simulate("click");
+  incrementBtn.simulate("click");
+  incrementBtn.simulate("click");
+  expect(wrapper.state().counter).toBe(counter + 3);
+});
+
+test("Decrement button render correctly", () => {
+  const wrapper = setup();
+  const decrementBtn = findByDataTest(wrapper, "decrement-btn");
+  expect(decrementBtn.length).toBe(1);
+});
+
+test("decrement button click decrement counter by 1", () => {
+  const wrapper = setup();
+  // const incrementBtn = findByDataTest(wrapper, "increment-btn");
+  // incrementBtn.simulate("click");
+  const decrementBtn = findByDataTest(wrapper, "decrement-btn");
+  // if (counter === 0) throw Error("counter cannot be less than zero");
+  decrementBtn.simulate("click");
+  const {
+    counter,
+    error: { countLessThanZero },
+  } = wrapper.state();
+  if (counter === 0) {
+    expect(countLessThanZero.length).toBeGreaterThan(2);
+  }
+  expect(counter).toBe(0);
+});
