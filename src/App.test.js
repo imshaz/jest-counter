49c8b7f51e9ead4c59799625e2382c835de77675
imshaz <imshazbangash@gmail.com>
2021-04-04 13:37:55 +0500

    code refactor

--- src/App.test.js
+++ src/App.test.js
@@ -6,24 +6,29 @@ import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
 
+const setup =(props={}, state=null)=>{
+  return shallow(<App {...props}/>);
+}
 
+const findByDataTest=(wrapper, datatest)=>{
+  return wrapper.find(`[data-test='${datatest}']`)
+}
 test(`render without any error`, ()=>{
-  const wrapper = shallow(<App/>);
-  const app=wrapper.find("[data-test='app']");
+  const wrapper = setup();
+  const app= findByDataTest(wrapper, 'app');
   expect(app.length).toBe(1)
 })
 
 
 test('render incremement button', ()=>{
-  const wrapper = shallow(<App/>);
-  const IncrementBtn=  wrapper.find("[data-test='increment-btn']")
-  
+  const wrapper = setup();
+  const IncrementBtn = findByDataTest(wrapper, 'increment-btn');  
   expect(IncrementBtn.length).toBe(1)
 })
 test('render conter placeholder', ()=>{
 
-  const wrapper = shallow(<App/>);
-  const countDisplay= wrapper.find("[data-test='counter-display']");
+  const wrapper = setup();
+  const countDisplay=  findByDataTest(wrapper, 'counter-display');
   expect(countDisplay.length).toBe(1)
 })
 test('counter start at 0', ()=>{})
