3de981e3f42007146ba8a49ed7a675471e173bda
imshaz <imshazbangash@gmail.com>
2021-04-04 12:30:21 +0500

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+
+import React , {Component} from "react"
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+class App extends Component {
+  constructor(props){
+    super(props)
+  }
+  render(){
+
+    return (
+      <div className="App">
+       <h1> Counter app </h1>
+      <div>current counter is </div>
+      <button>Increment</button>
+     
     </div>
   );
+}
 }
 
 export default App;
